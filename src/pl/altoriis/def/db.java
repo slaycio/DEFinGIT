b5436adf32e79899e9044cc2485f5409c59b7ad7
p. <p.>
2013-02-26 13:32:49 +0100

    dictionary moved to static_data class as ArrayList^3

--- src/pl/altoriis/def/db.java
+++ src/pl/altoriis/def/db.java
@@ -2,79 +2,66 @@ package pl.altoriis.def;
 
 import java.sql.*;
 
-
 public class db {
-	
+
 	public final properties properties = new properties();
 	public Connection cn = null;
 	public Statement st = null;
-	
-	 public db()
-	   {
-		   try {
-		  		
-		  		String url = this.properties.url;
-		  		String user = this.properties.user;
-		  		String password = this.properties.password;
-		  				  		
-		  		cn = DriverManager.getConnection(url, user, password);
-		  		st = cn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY); 
-		   }
-		   catch (SQLException ex) {
-					  ex.printStackTrace();
-		   }
-	   }
-	
-	
-	protected void finalize()
-	{
-		try {	
-			cn.close();	
-			}
-			catch (SQLException ex) {
-				  ex.printStackTrace();
-			}   	
-	}
-	
-			
+
+	public db() {
+		try {
+
+			String url = this.properties.url;
+			String user = this.properties.user;
+			String password = this.properties.password;
+
+			cn = DriverManager.getConnection(url, user, password);
+			st = cn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	} /* end of db() constructor method */
+
+	protected void finalize() {
+		try {
+			cn.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}/* end of finalize() method */
+
 	public String[][] get_data(String query, Integer columns) {
-		
+
 		ResultSet rs = null;
 		int rsSize = 0;
-        String[][] wynik = null;
-        
-		  try {
-	                       
-			  rs = st.executeQuery(query);
-		            
-			            try {
-			                rs.last();
-			                rsSize = rs.getRow();
-			                rs.beforeFirst();
-			            }
-			            catch(Exception ex) {
-			            	 ex.printStackTrace();
-			            }
-		            wynik = new String[rsSize][columns];
-		            
-		            while (rs.next())
-		            		{
-		            		for (int w = 0; w < columns; w++){
-		            	
-		            	    wynik[rs.getRow()-1][w] = rs.getString(w+1);
-		            		}          	    		            	
-		            		}
-		            		
-		           
-		            
-	            } catch (SQLException ex) {
-	            	 ex.printStackTrace();
+		String[][] wynik = null;
 
-	            } 
-		  		  
-		  	return wynik;
-			} // koniec metody
-	
-	
-}// koniec klasy
+		try {
 
+			rs = st.executeQuery(query);
+
+			try {
+				rs.last();
+				rsSize = rs.getRow();
+				rs.beforeFirst();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			wynik = new String[rsSize][columns];
+
+			while (rs.next()) {
+				for (int w = 0; w < columns; w++) {
+
+					wynik[rs.getRow() - 1][w] = rs.getString(w + 1);
+				}
+			}
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+
+		}
+
+		return wynik;
+	} /* end of get_data() method */
+
+}/* end of db class */
--- src/pl/altoriis/def/properties.java
+++ src/pl/altoriis/def/properties.java
@@ -9,43 +9,43 @@ import org.w3c.dom.Element;
 import java.io.*;
 
 public class properties {
-	
-	public  String url;
-	public  String user;
-    public  String password;
-	
-	properties (){
-				
+
+	public String url;
+	public String user;
+	public String password;
+
+	properties() {
+
 		try {
-			 
+
 			File fXmlFile = new File("src/pl/altoriis/def/properties.xml");
-			
+
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(fXmlFile);
-		    doc.getDocumentElement().normalize();
-		 
-			//System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-		 
+			doc.getDocumentElement().normalize();
+
+			// System.out.println("Root element :" +
+			// doc.getDocumentElement().getNodeName());
+
 			NodeList nList = doc.getElementsByTagName("database");
-		 					 
+
 			for (int temp = 0; temp < nList.getLength(); temp++) {
-		 
+
 				Node nNode = nList.item(temp);
-		 		 
+
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-		 
+
 					Element eElement = (Element) nNode;
-		
+
 					this.url = eElement.getElementsByTagName("url").item(0).getTextContent();
 					this.user = eElement.getElementsByTagName("user").item(0).getTextContent();
 					this.password = eElement.getElementsByTagName("password").item(0).getTextContent();
-					
+
 				}
 			}
-		    } catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		    }
-		  }
-	}
-
+		}
+	}/* end of properties() method*/
+}/* end of properties class */
